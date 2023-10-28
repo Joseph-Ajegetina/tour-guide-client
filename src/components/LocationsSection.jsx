1b593c93823fdc52b2040dc0e4789cff5b1fa798
Joseph-Ajegetina <josephajongbah@gmail.com>
2023-10-28 00:20:02 +0000

    add styles

--- src/components/LocationsSection.jsx
+++ src/components/LocationsSection.jsx
@@ -14,17 +14,18 @@ const cardStyle = {
 const textStyle = {
   position: "absolute",
   bottom: 10,
-  left: 150,
+  left: 50,
   padding: "8px",
   color: "beige",
   fontSize: "16px",
   fontWeight: "bold",
   fontFamily: "Century Gothic, sans-serif",
-  textShadow: "10px 10px 20px rgba(100, 100, 20, 1.5)",
-  width: "100%", // Take up the full width of the container
-  background: "rgba(0, 0, 0, 0.7)", // Semi-transparent background
+  textShadow: "10px 10px 10px rgba(100, 50, 20, 0.5)",
+  width: "60%", // Take up the full width of the container
+  background: "rgba(0, 0, 0, 0.2)", // Semi-transparent background
   borderRadius: "0 0 10px 10px", // Rounded corners only at the bottom // Adjust the shadow values as needed
 };
+
 
 const gradientOverlay = {
   position: "absolute",
