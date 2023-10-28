431823d52ee95b915cd3a5a1da3d6f6f8cb14780
Joseph-Ajegetina <josephajongbah@gmail.com>
2023-10-28 00:31:53 +0000

    make unforgettable experience worthwhile

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,39 +1,43 @@
 import React, { useState } from "react";
-import { Box, Image, Badge } from "@chakra-ui/react";
+import { Box, Image, Badge, LinkBox, LinkOverlay } from "@chakra-ui/react";
 
 function TourCard({ activity }) {
   return (
     activity && (
-      <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-        {<Image src={activity.images[0]} alt={activity.title} />}
+      <LinkBox>
+        <LinkOverlay href={`/activity/${activity._id}`}>
+          <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+            {<Image src={activity.images[0]} alt={activity.title} />}
 
-        <Box p="6">
-          <Box display="flex" alignItems="baseline">
-            <Badge borderRadius="full" px="2" colorScheme="teal">
-              New
-            </Badge>
-            <Box
-              color="gray.500"
-              fontWeight="semibold"
-              letterSpacing="wide"
-              fontSize="xs"
-              textTransform="uppercase"
-              ml="2"
-            ></Box>
-          </Box>
+            <Box p="6">
+              <Box display="flex" alignItems="baseline">
+                <Badge borderRadius="full" px="2" colorScheme="teal">
+                  New
+                </Badge>
+                <Box
+                  color="gray.500"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                ></Box>
+              </Box>
 
-          <Box
-            mt="1"
-            fontWeight="semibold"
-            as="h4"
-            lineHeight="tight"
-            noOfLines={1}
-          >
-            {activity.title}
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                noOfLines={1}
+              >
+                {activity.title}
+              </Box>
+              <Box>${activity.price}</Box>
+            </Box>
           </Box>
-          <Box>${activity.price}</Box>
-        </Box>
-      </Box>
+        </LinkOverlay>
+      </LinkBox>
     )
   );
 }
